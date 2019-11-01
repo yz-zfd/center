bd75fea5eb9a03370d5d3142b8516e16894593e1
zhangfada <564408462@qq.com>
2019-11-01 09:59:22 +0800

    Initial commit

---
+++ src/test/java/springboot/config/center/CenterApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.config.center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
